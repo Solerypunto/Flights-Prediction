2d89e99630aee37e7ebe2d75cf7c8f008d29ef5c
Sergio Soler Garcia <solerypunto@gmail.com>
2023-09-01 19:16:42 +0200

    Update 03_Predictor.py

--- pages/03_Predictor.py
+++ pages/03_Predictor.py
@@ -206,8 +206,8 @@ usos_horarios= {'Hawaii': uso_1, 'Alasca': uso_2, 'Washington': uso_3, 'Oregon':
 blktime_1 = datetime.datetime.strptime(str(int(franjahora.split('-')[1]) - random.randint(0, 59)), "%H%M").time()
 blktime_2 = (datetime.datetime.strptime(str(franjahora.split('-')[1]), "%H%M") + datetime.timedelta(minutes=random.randint(0, 59))).time()
 wheels_off = random.choice([blktime_1, blktime_2])
-st.write(wheels_off)
-wheels_on = wheels_off + datetime.timedelta(hours=usos_horarios.get(estadoorigen)-usos_horarios.get(estadodestino) ,minutes= tiempoaire)
+st.write(type(tiempoaire))
+wheels_on = wheels_off + datetime.timedelta(hours = (usos_horarios.get(estadoorigen) - usos_horarios.get(estadodestino) + tiempoaire))
 st.write(wheels_on)
 
 # WheelsOn = WheelsOff + HusoHorario + Airtime
