c64ffd00286a515a4c9bd57fd620fd91bca8e140
Germán Fdez <german.f.c.3@gmail.com>
2023-09-04 18:55:16 +0200

    update EDA

--- pages/02_EDA.py
+++ pages/02_EDA.py
@@ -8,12 +8,13 @@ import seaborn as sns
 from streamlit_option_menu import option_menu
 from streamlit_lottie import st_lottie
 import requests
+from PIL import Image
 
 ##### Configuracion de la página ###############################################################
 
-st.set_page_config(page_title= 'Flight delay predictor',
-                   page_icon= ':airplane_departure:',
-                   initial_sidebar_state= 'expanded', layout= 'wide',)
+st.set_page_config(page_title = 'Flight delay predictor',
+                   page_icon = ':airplane_departure:',
+                   initial_sidebar_state = 'expanded', layout = 'centered',)
 
 ## CSS ###############################################################   
 # tipografia   
@@ -45,7 +46,7 @@ df_all = pd.read_csv(path, sep=',', header= 0, )
 
 # dataset ppal
 path = "Data/dataset_categorical_001.parquet"
-df_big = pd.read_parquet(path).sample(20000)
+df_big = pd.read_parquet(path)
 df_big_mapa = df_big[['Origin', 'OriginCityName', 'FlightDate', 'Dest','LATITUDE', 'LONGITUDE', 'DepDelayMinutes', 'DepDelay']]
 
 # lat long
@@ -86,14 +87,61 @@ st_lottie(requests.get("https://lottie.host/e44dedf0-4f98-49e3-ab19-709275c763ae
 ##
 st.subheader('Dataset')
 st.write('''El dataset contiene multiples documentos, se pueden agrupar en datos en crudo y datos ya ordenados, Optamos por quedarnos con los ordenados:
-        6 documentos. Optamos por el formato *.parquet porque es mas ligero que *.csv \n
+        5 documentos. \n
             Tamaño total del dataset: 29.193.782 x 61''')
-st.write('Para complementar hemos creado las columnas de Latitud y Longitud para hacer gráficas')
+st.write("")
+
+# CARGA DE DATOS
+st.markdown('#### Carga de datos')
+
+imagen = Image.open("images/pyspark_data_load.png")
+st.image(imagen, use_column_width = True)
+
+st.write("Decidimos usar los archivos ```*.parquet``` porque eran más livianos que los ```*.csv```")
+st.write("")
+
+# STRINGINDEXER
+st.markdown('#### StringIndexer()')
+
+imagen = Image.open("images/string_indexer.png")
+st.image(imagen, use_column_width = True)
+
+st.write("Usamos ```StringIndexer()``` para transformar las columnas categóricas en numéricas.")
+st.write("Tras varias pruebas con los datos en el siguiente notebook, nos percatamos de que el modelo de keras nos arrojaba métricas con valores null, y encontramos que el problema se solventaba añadiendo al indexer el parámetro ```.setHandleInvalid('skip')```. De esta forma omitimos los nulos que creaba, por algún motivo, el ```StringIndexer()```.")
+st.write("")
+
+# PREPROCESAMIENTO
+st.markdown('#### Preprocesamiento de datos')
+
+st.write("Usar ```.toPandas()``` con pyspark provocaba colapsos de memoria y terminaba rompiendo el kernel. Decidimos usarlo con límites y filtros para visualizar de alguna manera los datos en un dataframe.")
+st.write("Para complementar hemos creado las columnas de ```latitud``` y ```longitud``` para hacer gráficas")
+
+expander = st.expander(label = "Latitud y longitud", expanded = True)
+
+with expander:
+    st.dataframe(df_lat_long, use_container_width = True)
+
+st.write("")
+
+# OUTLIERS
+st.markdown('#### Outliers')
+
+st.write('''En función a las gráficas que generamos más abajo decidimos filtrar el dataframe para eliminar posibles outliers y quedarnos con los valores relevantes. \n
+            Quitar outliers: df_all = df_all.filter(df_all["DepDelay"].between(-20, 200))''')
+
+# FIN
+st.write("")
+st.write("")
+
+st.write("Finalmente eliminamos columnas innecesarias y categóricas para dejar un DataFrame válido para usarlo en la predicción con el ```modelo de keras```.")
+
+imagen = Image.open("images/df_final_100%_real_no_fake.png")
+st.image(imagen, use_column_width = True)
 
 st.markdown("""<hr style="height:2px;border:none;color:#333;background-color:#ffe100;" /> """, unsafe_allow_html=True)
 
 ## Grafica 1
-st.subheader('Gráfica 1')
+st.subheader('Bubble chart')
 st.write( 'Para ver como se relaciona la media de retraso con el aeropuerto de origen. Cuanto mas grande sea el circulo mas alejado está de la media')
 
 col_a, col_b = st.columns([1,2])
@@ -138,6 +186,39 @@ with contenedor:
         sns.heatmap(corre, annot=True, ax=ax, cmap= 'plasma')
         st.pyplot(fig, use_container_width=True)
 
+st.markdown("""<hr style="height:2px;border:none;color:#333;background-color:#ffe100;" /> """, unsafe_allow_html=True)
+
+## Histograma
+
+st.subheader("Histograma")
+st.write("Esta fue la gráfica en la que nos basamos para eliminar los outliers, la gran mayoría de los datos se encontraba entre ```-20 y 200```. Todo esto ayudó a mejorar las métricas posteriores.")
+fig, ax = plt.subplots(nrows = 2, ncols = 2, figsize = (12, 12))
+
+# Primera columna
+ax[0, 0].hist(df_big[df_big["Operating_AirlineIndex"] == 0.0]["DepDelay"], bins = 100, color = "cyan")   
+
+ax[1, 0].hist(df_big[df_big["Operating_AirlineIndex"] == 1.0]["DepDelay"], bins = 100, color = "black")
+
+# Segunda columna
+ax[0, 1].hist(df_big[df_big["Operating_AirlineIndex"] == 21.0]["DepDelay"], bins = 100, color = "green")   
+
+ax[1, 1].hist(df_big[df_big["Operating_AirlineIndex"] == 13.0]["DepDelay"], bins = 100, color = "red")
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.pyplot()
+
+st.markdown("""<hr style="height:2px;border:none;color:#333;background-color:#ffe100;" /> """, unsafe_allow_html=True)
+
+## Pie
+
+st.subheader("Pie chart")
+st.write("Aquí visualizamos como afecta el retraso según la hora de salida de los vuelos. Una vez ```avanza``` el día, los retrasos se acumulan. Las horas ```posteriores a la mañana``` y ```previas a la noche``` no son aconsejables porque son más propensas a retrasos.")
+
+fig = px.pie(data_frame = df_big,
+             names = "DepTimeBlk",
+             values = "DepDelay")
+
+st.plotly_chart(figure_or_data = fig, use_container_width = True)
 
 st.markdown("""<hr style="height:2px;border:none;color:#333;background-color:#ffe100;" /> """, unsafe_allow_html=True)
 
