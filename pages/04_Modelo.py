f2e55bf4e3ccf923e58eaddd5ba95e45e00151d1
Sergio Soler Garcia <solerypunto@gmail.com>
2023-09-04 17:13:06 +0200

    cambios

---
+++ pages/04_Modelo.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+from streamlit_option_menu import option_menu
+from streamlit_lottie import st_lottie
+import requests
+import json
+import math
+import pickle as pkl
+from keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+import random
+
+##### Configuracion de la página ###############################################################
+
+st.set_page_config(page_title= 'Flight delay predictor',
+                   page_icon= ':airplane_departure:',
+                   initial_sidebar_state= 'expanded', layout= 'centered',)
+
+
+
+## CSS ###############################################################   
+# tipografia   
+streamlit_style = """
+			<style>
+			@import url('https://fonts.googleapis.com/css2?family=Space+Mono:wght@700&display=swap');
+
+			html, body, [class*="css"]  {
+			font-family: 'Space Mono', sans-serif;
+
+			}
+			</style>
+			"""
+st.markdown(streamlit_style, unsafe_allow_html=True)
+
+st.header('Modelo de predicción')
+
