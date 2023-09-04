85121e64751823b6c9fc03843f2c157e4ca2411e
Sergio Soler Garcia <solerypunto@gmail.com>
2023-09-04 11:34:04 +0200

    Update 03_Predictor.py

--- pages/03_Predictor.py
+++ pages/03_Predictor.py
@@ -247,20 +247,53 @@ latit_dest= df[df['DestCityName']==ciudaddest]['LATITUDE'].unique()[0]
 Y_RGB = [255, 255, 0, 40]
 G_RGB = [56, 191, 140, 40]
 
-st.pydeck_chart(pdk.Deck( map_style=None, 
-                         initial_view_state=pdk.ViewState(latitude=(longit - longit_dest),
-                                                          longitude= (latit - latit_dest), 
-                                                          zoom=2.6,
-                                                          pitch=0,),
-                         layers=[pdk.Layer("ArcLayer",
-                                            data= df,
-                                            get_width= 1,
-                                            get_source_position=[longit, latit],
-                                            get_target_position=[longit_dest, latit_dest],
-                                            get_tilt=1,
-                                            get_source_color=Y_RGB,
-                                            get_target_color=G_RGB,
-                                            pickable=True,
-                                            auto_highlight=True,
-                                            )]))
+# st.pydeck_chart(pdk.Deck( map_style=None, 
+#                          initial_view_state=pdk.ViewState(latitude=(longit - longit_dest),
+#                                                           longitude= (latit - latit_dest), 
+#                                                           zoom=2.6,
+#                                                           pitch=0,),
+#                          layers=[pdk.Layer("ArcLayer",
+#                                             data= df,
+#                                             get_width= 1,
+#                                             get_source_position=[longit, latit],
+#                                             get_target_position=[longit_dest, latit_dest],
+#                                             get_tilt=1,
+#                                             get_source_color=Y_RGB,
+#                                             get_target_color=G_RGB,
+#                                             pickable=True,
+#                                             auto_highlight=True,
+#                                             )]))
+
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
 
