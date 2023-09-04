89f32526b9820603bd5ad15ed148e7e5c2f6640c
Sergio Soler Garcia <solerypunto@gmail.com>
2023-09-04 12:49:38 +0200

    Update 03_Predictor.py

--- pages/03_Predictor.py
+++ pages/03_Predictor.py
@@ -280,7 +280,7 @@ CircleLayer= pdk.Layer("GreatCircleLayer",
 
 ArcLayers=pdk.Layer("ArcLayer",
                     data= df,
-                    get_width= 1,
+                    get_width= '100',
                     get_source_position=['longit', 'latit'],
                     get_target_position=['longit_desl', 'latit_dest'],
                     get_tilt=100,
