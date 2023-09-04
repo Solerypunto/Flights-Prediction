a57fde1d15bfa3cb52db6e9403820a3b6a5c5c9d
Sergio Soler Garcia <solerypunto@gmail.com>
2023-09-04 11:43:37 +0200

    Update 03_Predictor.py

--- pages/03_Predictor.py
+++ pages/03_Predictor.py
@@ -279,14 +279,14 @@ st.pydeck_chart(pdk.Deck(
     layers=[
         pdk.Layer(
            "ArcLayer",
-          data= df,
+            data= df,
             get_width= 1,
-            get_source_position=[longit, latit],
-            get_target_position=[longit_dest, latit_dest],
-            get_tilt=1,
-            get_source_color=Y_RGB,
-            get_target_color=G_RGB,
-            pickable=True,
+            get_source_position= [longit, latit],
+            get_target_position= [longit_dest, latit_dest],
+            get_tilt= 1,
+            # get_source_color= Y_RGB,
+            # get_target_color= G_RGB,
+            pickable= True,
             auto_highlight=True,
         ),
     #     pdk.Layer(
