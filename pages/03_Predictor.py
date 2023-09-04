a87fff7914febdb9a80162c086a7ea98291f421d
Sergio Soler Garcia <solerypunto@gmail.com>
2023-09-04 11:57:05 +0200

    Update 03_Predictor.py

--- pages/03_Predictor.py
+++ pages/03_Predictor.py
@@ -280,14 +280,13 @@ ArcLayer=[
     ),
 ],
 
-st.pydeck_chart(pdk.Deck(
+st.pydeck_chart(pdk.Deck(ArcLayer,
     map_style=None,
     initial_view_state=pdk.ViewState(
         latitude=38,
         longitude=-98,
         zoom=2.6,
         pitch=50,
-    ),
-    layers=ArcLayer
+    )
 ))
 
