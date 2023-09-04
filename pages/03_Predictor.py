97db49f5eec7f1a48193e14a227bc8ea82954200
Sergio Soler Garcia <solerypunto@gmail.com>
2023-09-04 13:07:55 +0200

    Update 03_Predictor.py

--- pages/03_Predictor.py
+++ pages/03_Predictor.py
@@ -245,8 +245,8 @@ longit_dest= df[df['Dest']==a_dest]['LONGITUDE'].unique()[0]
 latit_dest= df[df['Dest']==a_dest]['LATITUDE'].unique()[0]
 
 df_mapa = pd.DataFrame({'longit':longit, 'latit':latit, 'longit_dest':longit_dest, 'latit_dest':latit_dest}, index=[0])
-longit_media= ((longit+longit_dest)/2)
-latit_media= ((latit+latit_dest)/2)
+longit_media= round(((longit+longit_dest)/2), 4)
+latit_media= round(((latit+latit_dest)/2), 4)
 
 st.write(longit,latit,longit_dest,latit_dest, longit_media, latit_media)
 
