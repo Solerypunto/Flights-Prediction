448d9b876e1103a22b9fbb305eda3dea71898eb0
Sergio Soler Garcia <solerypunto@gmail.com>
2023-09-04 11:35:32 +0200

    Update 03_Predictor.py

--- pages/03_Predictor.py
+++ pages/03_Predictor.py
@@ -271,29 +271,29 @@ chart_data = pd.DataFrame(
 st.pydeck_chart(pdk.Deck(
     map_style=None,
     initial_view_state=pdk.ViewState(
-        latitude=37.76,
-        longitude=-122.4,
-        zoom=11,
+        latitude=(longit - longit_dest),
+        longitude=(latit - latit_dest),
+        zoom=2.6,
         pitch=50,
     ),
-    layers=[
-        pdk.Layer(
-           'HexagonLayer',
-           data=chart_data,
-           get_position='[lon, lat]',
-           radius=200,
-           elevation_scale=4,
-           elevation_range=[0, 1000],
-           pickable=True,
-           extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=chart_data,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-    ],
+    # layers=[
+    #     pdk.Layer(
+    #        'HexagonLayer',
+    #        data=chart_data,
+    #        get_position='[lon, lat]',
+    #        radius=200,
+    #        elevation_scale=4,
+    #        elevation_range=[0, 1000],
+    #        pickable=True,
+    #        extruded=True,
+    #     ),
+    #     pdk.Layer(
+    #         'ScatterplotLayer',
+    #         data=chart_data,
+    #         get_position='[lon, lat]',
+    #         get_color='[200, 30, 0, 160]',
+    #         get_radius=200,
+    #     ),
+    # ],
 ))
 
